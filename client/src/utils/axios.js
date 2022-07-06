451bd24782da85d8a9ba8634d16ca7e29859f333
irumvanselme <andesanselme@gmail.com>
2022-07-06 08:11:43 +0200

    feat: fix the indent ssues

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8000/";
 
 export function get(url, config = {}) {
-    return axios.get(url, config);
+	return axios.get(url, config);
 }
 
 export function post(url, body, config = {}) {
-    return axios.post(url, body, config);
+	return axios.post(url, body, config);
 }
